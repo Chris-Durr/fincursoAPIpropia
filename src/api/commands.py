5f116bd800d4d9e38b4c2512a3590d415d52f196
Chris-Durr <chrisdurr88@gmail.com>
2022-06-14 20:47:44 +0000

    2nd

--- src/api/commands.py
+++ src/api/commands.py
@@ -1,6 +1,6 @@
 
 import click
-from api.models import db, User
+from api.models import db, User, Fav_movie, Comment
 
 """
 In this file, you can add as many commands as you want using the @app.cli.command decorator
@@ -22,7 +22,6 @@ def setup_commands(app):
             user = User()
             user.email = "test_user" + str(x) + "@test.com"
             user.password = "123456"
-            user.is_active = True
             db.session.add(user)
             db.session.commit()
             print("User: ", user.email, " created.")
